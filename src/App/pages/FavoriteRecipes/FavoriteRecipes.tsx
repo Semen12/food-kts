835f852f4c8d0427799b68ebb0803fd6ee0ef911
Семён <kondaurov.semion@yandex.ru>
2024-11-24 20:54:48 +0300

    adding_chips

--- src/App/pages/FavoriteRecipes/FavoriteRecipes.tsx
+++ src/App/pages/FavoriteRecipes/FavoriteRecipes.tsx
@@ -1,5 +1,5 @@
 import { observer } from 'mobx-react-lite';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './FavoriteRecipes.module.scss';
 import { useFavoriteRecipes } from '@store/FavoriteRecipesStore/FavoriteRecipesContext';
 import CardGrid from '../RecipesList/components/CardGrid';
@@ -7,10 +7,12 @@ import ArrowRound from '@assets/arrow-round.svg?react';
 import { useNavigate } from 'react-router-dom';
 import Button from '@components/Button';
 import { Recipe } from '@store/types';
+import DeleteIcon from '@components/icons/DeleteIcon';
 
 const FavoriteRecipes = observer(() => {
   const favoriteStore = useFavoriteRecipes();
   const navigate = useNavigate();
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     favoriteStore.loadFromLocalStorage();
@@ -31,6 +33,10 @@ const FavoriteRecipes = observer(() => {
     </Button>
   );
 
+  const filteredFavorites = favoriteStore.favorites.filter(recipe => 
+    recipe.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <div className={styles.favorites}>
       <div className={styles.favorites__container}>
@@ -38,6 +44,26 @@ const FavoriteRecipes = observer(() => {
           <ArrowRound onClick={() => navigate(-1)}/>
           Your Favorite Recipes
         </h1>
+
+       {favoriteStore.favorites.length > 0 && <div className={styles.searchContainer}>
+          <div className={styles.inputWrapper}>
+            <input
+              type="text"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              placeholder="Поиск по избранным рецептам..."
+              className={styles.searchInput}
+            />
+            {searchQuery && (
+              <DeleteIcon 
+                className={styles.clearIcon} 
+                onClick={() => setSearchQuery('')}
+              />
+                
+             
+            )}
+          </div>
+        </div>}
 
         <div className={styles.favorites__content}>
           {favoriteStore.favorites.length === 0 ? (
@@ -47,9 +73,16 @@ const FavoriteRecipes = observer(() => {
                 <p>Сохраняйте понравившиеся рецепты, нажимая кнопку "Save" на карточках.</p>
               </div>
             </div>
+          ) : filteredFavorites.length === 0 ? (
+            <div className={styles.container}>
+              <div className={styles.noResults}>
+                <h3>Рецепты не найдены</h3>
+                <p>Попробуйте изменить поисковый запрос</p>
+              </div>
+            </div>
           ) : (
             <CardGrid 
-              recipes={favoriteStore.favorites} 
+              recipes={filteredFavorites}
               showSaveButton={false} 
               className={styles.favorites__grid}
               customActionSlot={renderRemoveButton}
