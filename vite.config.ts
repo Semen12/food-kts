9d19ad76aca64b13769fed682424c75da70b6210
Семён <kondaurov.semion@yandex.ru>
2024-11-28 13:40:49 +0300

    main

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
+  base: '/food-kts/',
   plugins: [react(), svgr()],
   resolve: {
     alias: {
