75a40733bd4f8d8548e3e6b24663585c0cea83f6
Семён <kondaurov.semion@yandex.ru>
2024-12-04 22:56:45 +0300

    Deploy new version

--- src/App/pages/RecipeDetails/components/RecipeNecessity/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './RecipeNecessity.tsx';
-
-
--- src/App/pages/RecipesList/types.ts
+++
@@ -1,6 +0,0 @@
-export const MEAL_TYPES = [
-    'main course', 'side dish', 'dessert', 'appetizer', 'salad', 'bread',
-    'breakfast', 'soup', 'beverage', 'sauce', 'marinade', 'fingerfood',
-    'snack', 'drink'
-  ].map(type => ({ key: type, value: type }));
-  
